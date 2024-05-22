42349a25ac9ca618f8e6071a231939b7414e37b2
uknowwho <florianvandersteen@gmail.com>
2024-05-22 09:54:01 +0200

    01: opdr 1, issue 2; eerste werkende versie van mi6 loginsysteem

--- src/main/java/nu/educom/MI6/Main.java
+++ src/main/java/nu/educom/MI6/Main.java
@@ -1,7 +1,35 @@
 package nu.educom.MI6;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    boolean[] blacklist = new boolean[999];
+    while (true) {
+      System.out.println("Welcome, id please:");
+      Scanner scanner = new Scanner(System.in);
+      StringBuilder agentInput = new StringBuilder(scanner.nextLine());
+
+      while (agentInput.length() > 3 || !(agentInput.toString().matches("^[0-9]+$")) || Integer.parseInt(agentInput.toString()) > 956 || Integer.parseInt(agentInput.toString()) <= 0 || blacklist[Integer.parseInt(agentInput.toString())]) {
+        System.out.println("Invalid, try again:");
+        agentInput = new StringBuilder(scanner.nextLine());
+      }
+
+      int agentId = Integer.parseInt(agentInput.toString());
+
+      while (agentInput.length() < 3) {
+        agentInput.insert(0, "0");
+      }
+
+      System.out.println("Hello there, " + agentInput);
+      StringBuilder agentPassphrase = new StringBuilder(scanner.nextLine());
+      if (!(agentPassphrase.toString().equals("For ThE Royal QUEEN"))) {
+        System.out.println("Sorry, " + agentInput + ", that is incorrect. You are now blacklisted.");
+        blacklist[agentId] = true;
+      }
+      else {
+        System.out.println("Welcome, " + agentInput + ", login successful.");
+      }
+    }
+
   }
 }
